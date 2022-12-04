5249fa8b060e3f61fe9efd8326638c39339704b6
MTalhaR <talhariaz021@gmail.com>
2022-12-05 00:05:59 +0500

    first commit

--- src/test/java/CucumberTool/RunnerClass.java
+++ src/test/java/CucumberTool/RunnerClass.java
@@ -1,2 +1,14 @@
-package CucumberTool;public class RunnerClass {
+package CucumberTool;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features =
+                "src\\test\\java\\CucumberTool\\Login.feature"
+)
+
+public class RunnerClass {
 }
--- src/test/java/steps/DashboardSteps.java
+++ src/test/java/steps/DashboardSteps.java
@@ -1,2 +1,11 @@
-package steps;public class DashboardSteps {
+package steps;
+
+import io.cucumber.java.en.Then;
+
+public class DashboardSteps {
+
+    @Then("user verify dashboard page")
+    public void user_verify_dashboard_page() {
+        System.out.println("Batch 14 is happy now");
+    }
 }
